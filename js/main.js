a46e21ee10e871e12596786da475e4da3041fcec
TheMartian0x48 <cr7.aditya.cs@gmail.com>
2022-05-17 18:12:05 +0530

    fixed bugs in tree class

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
+/*jshint esversion: 6 */
 "use strict";
 
 class TreeNode {
   constructor(name, url, isDirectory) {
-    this.name = this.setName(name);
-    this.url = this.setURL(url);
-    this.isDirectory = this.setIsDirectory(isDirectory);
-    this.parent = this.setParent(this.parent);
-    this.children = new Set();
+    this.name = name;
+    this.url = url;
+    this.isDirectory = isDirectory;
+    this.parent = null;
+    this.children = new Map();
   }
 
   setName(name) {
@@ -32,10 +33,10 @@ class TreeNode {
   }
 
   addChild(child) {
-    if (child == null || this.children.has(child)) {
+    if (this.children.has(child.name)) {
       return;
     }
-    this.children.add(child);
+    this.children.set(child.name, child);
     child.setParent(this);
   }
 };
@@ -45,30 +46,30 @@ class Tree {
     this.root = null;
     this.pwd = null;
   }
-  
+
   createTree(tree) {
     this.root = this.createTreeRecursively(tree, null);
     this.pwd = this.root;
   }
 
   createTreeRecursively(node, parent) {
-    validateNode(node);
-    let name = node["Name"];
-    let url = node["URL"];
+    this.validateNode(node);
+    let name = node["name"];
+    let url = node["url"];
     let isDirectory = node["isDirectory"];
     let newTreeNode = new TreeNode(name, url, isDirectory);
     newTreeNode.setParent(parent);
-    let children = node["Children"] === undefined ? null : node["Children"];
+    let children = node["children"] === undefined ? null : node["children"];
     if (isDirectory) {
-      for (let eachChild of children) {
-        if (eachChild.size != 0) {
-          this.createTreeRecursively(eachChild, newTreeNode);
+      for (let child of children) {
+        if (child.size != 0) {
+          this.createTreeRecursively(child, newTreeNode);
         }
       }
-    } else {
-      for (let eachChild of children) {
-        if (eachChild.size != 0) {
-          throw "File cann't have children";
+    } else if (!isDirectory && children != null){
+      for (let child of children) {
+        if (child.size != 0) {
+          throw "file can not have directories / files";
         }
       }
     }
@@ -76,28 +77,95 @@ class Tree {
   }
 
   validateNode(node) {
-    if (node instanceof Map == false) {
-      throw "validateNode : JSON Node must be Map Object";
-    }
-    let properties = ["Name", "URL", "isDirectory"];
-    for (let property in properties) {
+    let properties = ["name", "url", "isDirectory"];
+    for (let property of properties) {
       if (node[property] === undefined) {
         throw "validateNode : " + property + " not found for Name";
       }
     }
   }
+
+  isValidPathFromNode(path, node) {
+    //TODO
+  }
 };
 
 class Command {
-  constructor(manual) {
+  constructor(manual = null) {
+    if (manual == null) {
+      throw "Forget to set the manual";
+    }
     this.manual = manual;
   }
-  execute() {}
-  validateCommand(){}
+  execute(instruction) { }
+  validateCommand(argv) { }
+  printError(err) { }
 };
+
+// Global Object (must create singleton object)
+let fileSystem = new Tree();
+let supportedCommands = new Map();
 
 class Cd extends Command {
   constructor() {
-    super();
+    let manual = "<table><thead> <tr> <th>NAME</th> </tr></thead><tbody> <tr> <td>cd - change the working directory</td> </tr> <tr> <td>SYNOPSIS</td> </tr> <tr> <td>cd [directory]</td> </tr> <tr> <td>cd</td> </tr></tbody></table>";
+    super(manual);
+  }
+  
+  execute(instructions) {
+    let argv = instructions.split(" ").filter(instruction => instruction.length > 0);
+    if (!this.validateCommand(argv)) {
+      return;
+    }
+    if (argv.length == 1) {
+      fileSystem.pwd = fileSystem.root;
+    } else {
+      let pwd = fileSystem.pwd;
+      let folders = argv[1].split("/");
+      for (let folder in folders) {
+        if (folder == "..") {
+          pwd = pwd.parent;
+        } else if (folder != ".") {
+          pwd = pwd.children.get(folder);
+        }
+      }
+      fileSystem.pwd = pwd;
+    }
+  }
+  validateCommand(argv) {
+    if (argv.length == 1) {
+      return true;
+    };
+    if (argv.length == 2 && fileSystem.isValidPathFromNode(argv[1].split("/"), fileSystem.pwd)) {
+      return true;
+    }
+    super.printError("Incorrect Path");
+    return false;
   }
 }
+
+
+const url = "filesystem.json";
+// const url = "./old/web-terminal.json";
+
+function f() {
+  let a = 10;
+  let b = 20;
+  return a + b;
+}
+
+
+function Main() {
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    }).then((data) => {
+      fileSystem.createTree(data["root"]);
+    })
+    .catch(function (err) { });
+ 
+  // supportedCommands.set("cd", new Cd());
+  // console.log(f());
+}
+
+Main();
